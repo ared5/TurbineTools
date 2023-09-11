ce6078b54405ccdf292f6d5f9fcea1e45eb18e33
ared5 <aitorredondoruiz@gmail.com>
2023-09-12 00:11:00 +0200

    Add SPC functionality

--- Turbine.m
+++ Turbine.m
@@ -6,30 +6,36 @@ classdef Turbine
         turbineName 
         powerDesign
         rotorSpeedDesign 
-        optipitch             
+          
+        speedCurve
+        powerCurve
     end
 
     properties (Access = public)
-        
+        optipitch
     end
     
-    methods
-        function obj = Turbine(turbineName, powerDesign, rotorSpeedDesign, optipitch)
-            %TURBINE Construct an instance of this class
-            %   Detailed explanation goes here
-
-            arguments
-                turbineName (1, :) char {mustBeTextScalar} = 'NoName'
-                powerDesign (1, :) double {mustBeNumeric} = 0
-                rotorSpeedDesign (1, :) double {mustBeNumeric} = 0
-                optipitch (2, 5)  = [0 0 0 0 0; 0 0 0 0 0]
-            end
-
-            obj.turbineName = turbineName;
-            obj.powerDesign = powerDesign;
-            obj.rotorSpeedDesign = rotorSpeedDesign;
-            obj.optipitch = optipitch;            
-        end
+    methods 
+        % function obj = Turbine(turbineName, powerDesign, rotorSpeedDesign, optipitch, speedCurve, powerCurve)
+        %     %TURBINE Construct an instance of this class
+        %     %   Detailed explanation goes here
+        % 
+        %     arguments
+        %         turbineName (1, :) char {mustBeTextScalar} = 'NoName'
+        %         powerDesign (1, :) double {mustBeNumeric} = 0
+        %         rotorSpeedDesign (1, :) double {mustBeNumeric} = 0
+        %         optipitch (2, 5)  = [0 0 0 0 0; 0 0 0 0 0]
+        %         speedCurve (1, 6) double {mustBeVector} = [0 0 0 0 0 0]
+        %         powerCurve (1, 6) double {mustBeVector} = [0 0 0 0 0 0]
+        %     end
+        % 
+        %     obj.turbineName = turbineName;
+        %     obj.powerDesign = powerDesign;
+        %     obj.rotorSpeedDesign = rotorSpeedDesign;
+        %     obj.optipitch = optipitch;       
+        %     obj.speedCurve = speedCurve;
+        %     obj.powerCurve = powerCurve;
+        % end
 
         function optipit = get_optipitch(obj)
             optipit = obj.optipitch;
@@ -41,6 +47,15 @@ classdef Turbine
 
         function obj = set_optipitch(obj, optipitch)
             obj.optipitch = optipitch;
+        end
+
+        function obj = set_SPC(obj, speedCurve, powerCurve)
+            obj.speedCurve = speedCurve;
+            obj.powerCurve = powerCurve;
+        end
+
+        function SPC = get_SPC(obj)
+            SPC = [obj.speedCurve; obj.powerCurve];
         end
 
     end
